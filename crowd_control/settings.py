47cc856c0627dc846a22524b2db8936385109369
Anis Sakkaf <anisanissakkaf@gmail.com>
2018-04-18 10:05:34 +0200

    Reverted database to sqlite3

--- crowd_control/settings.py
+++ crowd_control/settings.py
@@ -74,11 +74,8 @@ WSGI_APPLICATION = 'crowd_control.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'user',
-        'PASSWORD': 'password',
-        'NAME': 'test',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
