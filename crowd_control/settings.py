f85102b2a6f3d9a000b07aa0d7284785ad1cccc1
Robin Van den Broeck <robin.broeck@gmail.com>
2018-04-18 14:56:58 +0200

    Fixed cors

--- crowd_control/settings.py
+++ crowd_control/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'crowd_control.urls'
@@ -69,10 +69,7 @@ TEMPLATES = [
     },
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    'localhost',
-    'ucll-cc-frontend.herokuapp.com'
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
 WSGI_APPLICATION = 'crowd_control.wsgi.application'
 
